e4bcebe6afa5c3d4eee881e7d82b668a32103d9c
Noor Ullah Al Noor <nurullahan408@gmail.com>
2023-11-05 11:48:12 +0600

    Uploading final project except the readme

---
+++ index.js
@@ -0,0 +1,114 @@
+//Get input labels
+let allLabels = document.getElementsByTagName('label');
+let labelsArr = [allLabels[0],allLabels[1],allLabels[2]];
+console.log(labelsArr)
+// Get input day, month, year
+let userDay = document.getElementById("day");
+let userMonth = document.getElementById("month");
+let userYear = document.getElementById("year");
+let userInputs = [userDay,userMonth,userYear];
+// Get the warning spans
+let warningDay = document.getElementById('warning-day');
+let warningMonth = document.getElementById('warning-month');
+let warningYear = document.getElementById('warning-year');
+let warnings = [warningYear,warningMonth,warningDay];
+// Get the result spans 
+let resultYears = document.getElementById('result-years');
+let resultMonths = document.getElementById('result-months');
+let resultDays = document.getElementById('result-days');
+//function to validate the date
+function validateDate(year,month,date) {
+    let userDate = new Date(year,month - 1,date);
+    return userDate.getFullYear() === year && userDate.getMonth() === (month-1) && userDate.getDate() === date;
+}
+//function to animate the numbers
+function AgeCounter(DomElement, limit){
+    let i=0;
+    const counting = setInterval(Increment,90);
+    function Increment() {
+        i = i + 1;
+        DomElement.textContent = i;
+        if(i >= limit){
+            clearInterval(counting);
+        }
+    }
+    return DomElement.textContent;
+}
+//function to find the age 
+function getAge(year,month,day){
+    let userDate = new Date(year,month,day);
+    let dateToday = new Date();
+    let age_year = dateToday.getFullYear() - userDate.getFullYear();
+    // get months
+    let age_month = 0;
+    let age_date = 0;
+    if(dateToday.getMonth() >= userDate.getMonth()){
+        age_month = dateToday.getMonth() - userDate.getMonth();
+    } else {
+        age_year--;
+        age_month = 12 + (dateToday.getMonth() - userDate.getMonth());
+    }
+    // get the days
+    if(dateToday.getDate() >= userDate.getDate()){
+        age_date = dateToday.getDate() - userDate.getDate();
+    } else {
+        age_month--;
+        age_date = 31 + (dateToday.getDate() - userDate.getDate());
+    }
+    if (age_month < 0){
+        age_month = 11;
+        age_year--;
+    }
+    return [age_year, age_month + 1, age_date];
+}
+// result variables
+let res_year = 0;
+let res_month = 0;
+let res_day = 0;
+//Get the form by id
+let ageForm = document.getElementById("ageForm");
+//Add an event listener to the form
+let user_day = 0;
+let user_month = 0;
+let user_year = 0;
+ageForm.addEventListener("submit",(e) => {
+    e.preventDefault();
+    //perform checks on the value
+    user_day = Number(userDay.value);
+    user_month = Number(userMonth.value);
+    user_year = Number(userYear.value);
+    // validation check on the input
+    if(validateDate(user_year,user_month,user_day) === false){
+        warnings.map((e) => {
+            e.textContent = "Must be valid ";
+            e.classList.add("warning");
+        })
+        userInputs.map((e) => {
+            e.classList.add("warning-input");
+        })
+        labelsArr.map((e) => {
+            e.classList.add("warning-label")
+        })
+        warningDay.textContent += "day"; 
+        warningMonth.textContent += "month"; 
+        warningYear.textContent += "year";
+    } else {
+        warnings.map((e) => {
+            e.classList.remove("warning");
+        })
+        userInputs.map((e) => {
+            e.classList.remove("warning-input");
+        })
+        labelsArr.map((e) => {
+            e.classList.remove("warning-label")
+        })
+        warningDay.textContent = ""; 
+        warningMonth.textContent = ""; 
+        warningYear.textContent = "";
+        [res_year,res_month,res_day] = getAge(user_year,user_month,user_day);
+        resultYears.textContent = AgeCounter(resultYears,res_year);
+        resultMonths.textContent = AgeCounter(resultMonths,res_month);
+        resultDays.textContent = AgeCounter(resultDays,res_day);
+    }
+     
+})
